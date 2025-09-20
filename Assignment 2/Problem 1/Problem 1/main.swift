3f301364f3959cc23ca9cd92a9f3ac1f3780917e
Ayau Kuat <ayaulim1002k@gmail.com>
2025-09-20 19:36:20 +0500

    Problem 1 Assignment 2

---
+++ Assignment 2/Problem 1/Problem 1/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Problem 1
+//
+//  Created by Аяулым Куат on 20.09.2025.
+//
+
+import Foundation
+
+for index in 1...100{
+    if index % 3 == 0 {print("Fizz")}
+    else if index % 5 == 0 {print("Buzz")}
+    else if index % 3 == 0 && index % 5 == 0 {print("FizzBuzz")}
+    else {print(index)}
+}

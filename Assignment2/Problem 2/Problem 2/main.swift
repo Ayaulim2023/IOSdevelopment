dc93ba11a9d476e3a848ede48b598d3e23083496
Ayau Kuat <ayaulim1002k@gmail.com>
2025-09-20 19:54:15 +0500

    Problem 2 Assignment 2

---
+++ Assignment2/Problem 2/Problem 2/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Problem 2
+//
+//  Created by Аяулым Куат on 20.09.2025.
+//
+
+import Foundation
+
+func isPrime(_ n: Int) -> Bool {
+    if n < 2 {
+        return false
+    }
+    for i in 2..<n {
+        if n % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+for i in 1...100{
+    if(isPrime(i)==true){print(i)}
+}
